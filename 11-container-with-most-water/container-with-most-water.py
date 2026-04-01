a601d2cc758b9095f075fb1be0d9f72691041914
U.Edukondalu <uedukondal6@gmail.com>
2026-04-01 19:44:17 +0530

    Time: 119 ms (56.06%) | Memory: 20.7 MB (42.60%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l=0
+        r=len(height)-1
+        m=0
+        while l<r:
+            h=min(height[l],height[r])
+            w=r-l
+            a=h*w
+            m=max(m,a)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+        return m
