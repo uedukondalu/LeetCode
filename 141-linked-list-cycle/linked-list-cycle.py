81517e4955574cb902d8a7875a432a1731d80bb5
U.Edukondalu <uedukondal6@gmail.com>
2026-05-06 16:09:48 +0530

    Time: 36 ms (60.95%) | Memory: 19.5 MB (30.26%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def hasCycle(self, head):
+        slow=fast=head
+        while fast!=None and fast.next!=None:
+            fast=fast.next.next
+            slow=slow.next
+            if fast==slow:
+                return True
+        return False
