1397fe3d585e8250cbd0b86ff19f68e5c1e98a12
U.Edukondalu <uedukondal6@gmail.com>
2026-05-22 11:09:01 +0530

    Time: 195 ms (1.64%) | Memory: 12.9 MB (6.15%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -2,7 +2,18 @@ class Solution(object):
     def search(self, nums, target):
         t=target
         n=sorted(nums)
+        f=[]
         for i in range(len(n)):
-            if n[i]==target:
+            if n[i] not in f:
+                f.append(n[i])
+        l=0
+        r=len(f)-1
+        while l<=r:
+            m=(l+r)//2
+            if f[m]==t:
                 return True
+            elif f[m]<t:
+                l=m+1
+            else:
+                r=m-1
         return False
