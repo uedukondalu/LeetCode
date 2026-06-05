77676789391bccdc770470bc689412f8293f1ebc
U.Edukondalu <uedukondal6@gmail.com>
2026-06-05 17:34:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (56.33%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        a=1
+        b=2
+        for i in range(3,n+1):
+            a,b=b,a+b    
+        return b
