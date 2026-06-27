e14ced1c746fa63cb3785f24552f2d2525125597
U.Edukondalu <uedukondal6@gmail.com>
2026-06-27 14:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.16%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def lastStoneWeight(self, stones):
         s=stones
-        while(len(s)>1):
+        while len(s)>1:
             s.sort(reverse=True)
-            y=s.pop(0)
-            x=s.pop(0)
-            if y!=x:
-                s.append(y-x)
+            q=s.pop(0)
+            p=s.pop(0)
+            if q!=p:
+                s.append(q-p)
         if s:
             return s[0]
         return 0
+
 
