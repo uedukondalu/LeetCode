b64401a1e5db236508dbecefa4ca20c2b8fa6ad0
U.Edukondalu <uedukondal6@gmail.com>
2026-07-03 20:51:24 +0530

    Time: 3 ms (45.43%) | Memory: 12.5 MB (23.92%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i][j]!=matrix[i-1][j-1]:
+                    return False
+        return True
+        
